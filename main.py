ac5ec5c619990c0ee76f49213d4ec313855542f3
hasanozz <hasanozz91@gmail.com>
2023-07-18 02:07:07 +0300

    edit

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import os
 should_continue = True
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 my_cards = []
-computer_cards = []
+dealer_cards = []
 
 def get_card():
     my_value = sum(my_cards)
-    computer_value = sum(computer_cards)
+    dealer_value = sum(dealer_cards)
     print(f"\nYour hand {my_cards}, current score = {my_value}")
-    print(f"Dealers hand [{computer_cards[0]}, ?]\n")
+    print(f"Dealers hand [{dealer_cards[0]}, ?]\n")
     another_card = input("Type 'y' to Hit or type anything to Stand: ").lower()
 
     if another_card == 'y':
@@ -27,33 +27,33 @@ def get_card():
             else:
                 print_scores("BUST!\nDEALER WINS!")
     else:
-        while computer_value < 17:
-            computer_cards.append(random.choice(cards))
-            computer_value = sum(computer_cards)
+        while dealer_value < 17:
+            dealer_cards.append(random.choice(cards))
+            dealer_value = sum(dealer_cards)
 
-            if computer_value > 21:
-                if 11 in computer_cards:
-                    computer_cards.pop(computer_cards.index(11))
-                    computer_cards.append(1)
-                    computer_value = sum(computer_cards)    
+            if dealer_value > 21:
+                if 11 in dealer_cards:
+                    dealer_cards.pop(dealer_cards.index(11))
+                    dealer_cards.append(1)
+                    dealer_value = sum(dealer_cards)    
 
-        if computer_value > 21:    
+        if dealer_value > 21:    
             print_scores("DEALER BUSTS\nYOU WIN!")
             
-        elif computer_value == my_value:
+        elif dealer_value == my_value:
             print_scores("PUSH!")
 
-        elif computer_value < my_value:    
+        elif dealer_value < my_value:    
             print_scores("YOU WIN!")
 
-        elif computer_value > my_value:    
+        elif dealer_value > my_value:    
             print_scores("DEALER WINS!")
 
 def print_scores(status):
     my_value = sum(my_cards)
-    computer_value = sum(computer_cards)
+    dealer_value = sum(dealer_cards)
     print(f"Your hand {my_cards}. Your final score = {my_value}")
-    print(f"Dealers hand {computer_cards}. Dealers final score = {computer_value}\n")
+    print(f"Dealers hand {dealer_cards}. Dealers final score = {dealer_value}\n")
     print(status)
 
 while should_continue:
@@ -64,18 +64,18 @@ while should_continue:
     os.system('cls')
     print(art.logo)
     my_cards = random.sample(cards, 2)
-    computer_cards = random.sample(cards, 2)
+    dealer_cards = random.sample(cards, 2)
     my_value = sum(my_cards)
-    computer_value = sum(computer_cards)
+    dealer_value = sum(dealer_cards)
 
-    if my_value == 21 and computer_value == 21:
+    if my_value == 21 and dealer_value == 21:
         print_scores("You both have Blackjack\nPUSH!")
 
     elif my_value == 21:
         print_scores("You have Blackjack\nYOU WIN!")
 
-    elif computer_value == 21:
-        print_scores("Computer have Blackjack\nDEALER WINS!")
+    elif dealer_value == 21:
+        print_scores("Dealer have Blackjack\nDEALER WINS!")
 
     else:
         get_card()
